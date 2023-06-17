6d930ec28bcee0da6ded3df22b11ac7365789e61
UTXOnly <brian.hartford@datadoghq.com>
2023-06-17 01:48:51 -0400

    cleanup

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-java -Dcom.sun.management.jmxremote \
-     -Dcom.sun.management.jmxremote.port=8001 \
-     -Dcom.sun.management.jmxremote.authenticate=false \
-     -Dcom.sun.management.jmxremote.ssl=false \
-     SimpleWebServer
-     #SimpleAgent
